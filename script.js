96a4c4707fc044965deedff2a1c070c051c53f53
jacopolegovini <legovini96@gmail.com>
2024-08-02 14:53:07 +0200

    milestone 1 completed

--- script.js
+++ script.js
@@ -9,4 +9,35 @@ console.log('JS OK');
 //  - Se % di tre e cinque da resto 0 allora è divisibile
 //  - Stampa il risultato
 
+// Dichiarazione variabili
+const fizz = 'Fizz';
+const buzz = 'Buzz';
+const fizzBuzz = 'FizzBuzz';
+let textPrint = '';
 
+// Ciclo for
+for (i = 1; i <= 100; i++) {
+    textPrint = i;
+
+    if ((i % 3) === 0) {
+        if ((i % 5) === 0) {
+            textPrint = fizzBuzz;
+            console.log(textPrint);
+        } else {
+            textPrint = fizz;
+            console.log(textPrint);
+        }
+
+    } else if ((i % 5) === 0) {
+        if ((i % 3) === 0) {
+            textPrint = fizzBuzz;
+            console.log(textPrint);
+        } else {
+            textPrint = buzz;
+            console.log(textPrint);
+        }
+
+    } else {
+        console.log(textPrint);
+    }
+}
