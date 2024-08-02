d08ddbaeda93e0a5dffb1e9018c88d203611fc47
jacopolegovini <legovini96@gmail.com>
2024-08-02 14:33:46 +0200

    comments milestone 1

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('JS OK');
+
+// MILESTONE 1
+// Dichiarazione variabili
+// Ciclo for
+//  - Annidamento if
+//  - Se % di tre da resto 0 allora è divisibile
+//  - Se % di cinque da resto 0 allora è divisibile
+//  - Se % di tre e cinque da resto 0 allora è divisibile
+//  - Stampa il risultato
+
+
